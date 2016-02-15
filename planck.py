e2e3acf618de532788079c86aba7aadd7152a585
Saroj Adhikari <adh.saroj@psu.edu>
2016-02-15 11:05:52 -0500

    add planck 2013 and 2015 cosmologies in planck.py

---
+++ planck.py
@@ -0,0 +1,44 @@
+"""
+cosmology.cosmo class with Planck 2013 and 2015 cosmological parameters
+"""
+
+from cosmology import cosmo
+
+class Planck2013(cosmo):
+    def __init__(self):
+        self.Ob0=0.048252; self.Om0=0.30712
+        self.H0=67.77
+        self.n=0.9611; self.r=0.
+        self.sigma8=0.8288
+        self.tau=0.0952; self.z_reion=11.52
+        self.t0=13.7965; self.Tcmb0=2.7255
+        self.Neff=3.046
+        self.flat=True
+        self.m_nu=[0., 0., 0.06]
+        self.f_baryon=self.Ob0/self.Om0
+        self.h=self.H0/100.
+        self.A=1.0
+        self.fnl=0.0
+
+        self.normalize() # normalize the primordial amplitude A for the given sigma8
+
+class Planck2015(cosmo):
+    def __init__(self):
+        self.Ob0=0.048252; self.Om0=0.308
+        self.H0=67.8
+        self.n=0.968
+        self.r=0.
+        self.sigma8=0.815
+        self.tau=0.0952
+        self.z_reion=11.52
+        self.t0=13.7965
+        self.Tcmb0=2.7255
+        self.Neff=3.046
+        self.flat=True
+        self.m_nu=[0., 0., 0.06]
+        self.f_baryon=self.Ob0/self.Om0
+        self.h=self.H0/100.
+        self.A=1.0
+        self.fnl=0.0
+
+        self.normalize()
