e610da223067223e4b7decfe36c08f66953bb92b
Saroj Adhikari <adh.saroj@gmail.com>
2016-09-15 14:53:08 -0400

    add the l**2 scaling for E transfer function

--- cosmology.py
+++ cosmology.py
@@ -109,19 +109,26 @@ class cosmo(object):
         # check if klist and glk are already loaded
         if (len(self.klist)>0 and len(self.glk)>0):
             if (TEB==0):
-                return self.klist, self.glk[0, l-2, :]
-            if (TEB==1):
-                return self.klist, l*l*self.glk[1, l-2, :]]
+                return self.glk[0, l-2, :]
+            else:
+                return l*l*self.glk[TEB, l-2, :]
         else:
             # first check if there is a file saved for the current AccuracyBoost and LMAX
             fname = "glk_"+self.name+"_"+str(self.camb_aboost)+"_"+str(self.camblmax)+".npy"
             if (isfile(fname)):
                 print ("cmb transfer saved file found!")
                 self.klist, self.glk = np.load(fname)
+                if (TEB==0):
+                    return self.glk[0, l-2, :]
+                else:
+                    return l*l*self.glk[TEB, l-2, :]
             else:
                 print ("cmb transfer file not found...generating")
                 self.init_camb_transfer()
-        self.get_cmb_transfer_l(TEB, l)
+                if (TEB==0):
+                    return self.glk[0, l-2, :]
+                else:
+                    return l*l*self.glk[TEB, l-2, :]
 
     def set_r(self, r):
         self.r=r
